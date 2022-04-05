c86a1d384ce04b4676848d7b95f364450e62998b
JavierxHernandez <Javierxhernandezx@gmail.com>
2022-04-05 14:47:15 -0400

    Create navigation bar and footer layouts

--- resources/views/layout/navbar-item.blade.php
+++ resources/views/layout/navbar-item.blade.php
@@ -1,3 +1,6 @@
-<div>
-    <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
-</div>
+<li>
+    <a href="{{ $href }}"
+        class="text.xs font-medium text-dark py-2 flex hover:text-primary dark:text-gray-200 lg-inline-flex lg:ml-6 xl:ml-12">
+        {{ $slot }}
+    </a>
+</li>
